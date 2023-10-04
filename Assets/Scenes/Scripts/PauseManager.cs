cc717518fc2d5634e8e2f9353d73df2c1822712b
eduardoacost <andres_edu.acosta@uao.edu.co>
2023-10-04 16:30:24 -0500

    menu pausa hecho + seguimiento de camara

--- Assets/Scenes/Scripts/PauseManager.cs
+++ Assets/Scenes/Scripts/PauseManager.cs
@@ -2,52 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-     public GameObject pausaCanvas;
+    public GameObject pausaCanvas; // Arrastra el Canvas de pausa aquí
     private bool isPaused = false;
+    private Transform playerCamera; // Cámara del jugador
+    private float distanciaDeseada = 2.0f; // Cambia este valor según tus necesidades
+
+    public InputActionProperty actionProperty;
+
+    private void Awake()
+    {
+        actionProperty.action.performed += OnButtonPressed;
+    }
+
+    private void OnEnable()
+    {
+        actionProperty.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        actionProperty.action.Disable();
+    }
 
     void Start()
     {
         pausaCanvas.SetActive(false);
+        playerCamera = Camera.main.transform; // Obtén la cámara principal como la cámara del jugador
     }
 
     void Update()
     {
-        // Comprueba si la tecla "P" ha sido presionada para pausar o reanudar el juego.
+        // Comprueba si la tecla "P" ha sido presionada para mostrar o ocultar el menú de pausa.
         if (Keyboard.current.pKey.wasPressedThisFrame)
         {
             TogglePausa();
         }
+    }
 
-        // Comprueba si el botón "A" del joystick izquierdo se ha presionado para pausar o reanudar el juego.
-        if (Gamepad.all.Count > 0)
+    private void OnButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.ReadValueAsButton())
         {
-            Gamepad gamepad = Gamepad.all[0]; // Esto asume que hay al menos un controlador conectado.
-
-            // Comprueba si el botón "A" del joystick izquierdo ha sido presionado.
-            if (gamepad.buttonSouth.wasPressedThisFrame) // "buttonSouth" es el botón "A".
-            {
-                TogglePausa();
-            }
+            TogglePausa();
         }
     }
 
     void TogglePausa()
     {
         isPaused = !isPaused;
-        pausaCanvas.SetActive(isPaused);
 
-        Time.timeScale = isPaused ? 0 : 1; // Pausa el tiempo en el juego cuando esté pausado.
+        if (isPaused)
+        {
+            // Coloca el Canvas frente al jugador.
+            pausaCanvas.transform.position = playerCamera.position + playerCamera.forward * distanciaDeseada;
+            pausaCanvas.transform.rotation = playerCamera.rotation;
+
+            // Activa el Canvas de pausa.
+            pausaCanvas.SetActive(true);
+
+            // Bloquea el movimiento y rotación del jugador (ajusta esto según tu controlador de movimiento).
+            // Ejemplo: playerController.LockMovementAndRotation();
+        }
+        else
+        {
+            // Desactiva el Canvas de pausa.
+            pausaCanvas.SetActive(false);
+
+            // Reanuda el movimiento y rotación del jugador (ajusta esto según tu controlador de movimiento).
+            // Ejemplo: playerController.UnlockMovementAndRotation();
+        }
     }
 
     public void Continuar()
     {
         TogglePausa();
     }
+
     // Cargar el menú principal
     public void SalirAlMenuPrincipal()
     {
@@ -60,9 +94,6 @@ public class PauseManager : MonoBehaviour
         // Puedes almacenar el nombre de la escena anterior en una variable antes de cambiar de escena y usarla aquí.
         SceneManager.LoadScene("Bienvenido"); // Reemplaza "NombreDeLaEscenaAnterior" con el nombre de tu escena anterior.
     }
-
-
-
-   
 }
+
 
